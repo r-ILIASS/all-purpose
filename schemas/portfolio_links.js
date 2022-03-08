58d0761395904e55c37ed07419fcad8924a48a3a
r-ILIASS <iliass.root@gmail.com>
2022-03-08 14:46:39 +0100

    feat[portfolio]: add schemas for links and projects

---
+++ schemas/portfolio_links.js
@@ -0,0 +1,12 @@
+export default {
+  name: "portfolio_links",
+  title: "Portfolio Links",
+  type: "document",
+  fields: [
+    {
+      name: "resumeLink",
+      title: "Resume Link",
+      type: "string",
+    },
+  ],
+};
---
+++ schemas/portfolio_projects.js
@@ -0,0 +1,44 @@
+export default {
+  name: "portfolio_projects",
+  title: "Portfolio Projects",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+    },
+    {
+      name: "mainImage",
+      title: "Main image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "string",
+    },
+    {
+      name: "source",
+      title: "Source",
+      type: "string",
+    },
+    {
+      name: "live",
+      title: "Live",
+      type: "string",
+    },
+  ],
+};
