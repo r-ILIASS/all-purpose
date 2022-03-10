5d95ffbbc78b1fafc36ef1a7ca7d3439e47ce42b
r-ILIASS <iliass.root@gmail.com>
2022-03-10 14:10:22 +0100

    feat[portfolio_projects.js]: add order field

--- schemas/portfolio_projects.js
+++ schemas/portfolio_projects.js
@@ -9,6 +9,11 @@ export default {
       type: "string",
     },
     {
+      name: "order",
+      title: "Order",
+      type: "number",
+    },
+    {
       name: "slug",
       title: "Slug",
       type: "slug",
